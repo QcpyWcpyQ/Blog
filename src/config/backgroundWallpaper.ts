9c2f4d20059d3b924ddb10216a693e6e40bea9b1
QcpyWcpyQ <2587745920@qq.com>
2026-09-18 21:06:08 +0800

    update

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -59,17 +59,19 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否启用主页横幅文字
 			enable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "Lovely Keqing!",
 			// 主页横幅主标题字体大小
 			titleSize: "4.5rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+                "Sometimes I want to break all of the rules.",
+                "Sometimes I don't want to silence my youth.",
+                "Sometimes I feel like there's nothing to lose",
+                "Sometimes it's best to run wild.",
+                "We won't be silent.",
+                "We won't do what we're told.",
+                "We are still kids.",
+                "We refuse to grow old.",
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
